c98bec90b606f31ef54dd8a6ee2588eef9e4b873
Ronald A. Richardson <me@ron.dev>
2025-12-21 17:44:38 +0800

    Migrate to new extension architecture

--- addon/engine.js
+++ addon/engine.js
@@ -2,38 +2,15 @@ import Engine from '@ember/engine';
 import loadInitializers from 'ember-load-initializers';
 import Resolver from 'ember-resolver';
 import config from './config/environment';
-import services from '@fleetbase/ember-core/exports/services';
-import AdminSolidServerConfigComponent from './components/admin/solid-server-config';
-import SolidBrandIconComponent from './components/solid-brand-icon';
+import { services, externalRoutes } from '@fleetbase/ember-core/exports';
 
 const { modulePrefix } = config;
-const externalRoutes = ['console', 'extensions'];
-
 export default class SolidEngine extends Engine {
     modulePrefix = modulePrefix;
     Resolver = Resolver;
     dependencies = {
         services,
         externalRoutes,
-    };
-    setupExtension = function (app, engine, universe) {
-        // register menu item in header
-        universe.registerHeaderMenuItem('Solid', 'console.solid-protocol', { iconComponent: SolidBrandIconComponent, iconComponentOptions: { width: 19, height: 19 }, priority: 5 });
-
-        // register admin settings -- create a solid server menu panel with it's own setting options
-        universe.registerAdminMenuPanel(
-            'Solid Protocol',
-            [
-                {
-                    title: 'Solid Server Config',
-                    icon: 'sliders',
-                    component: AdminSolidServerConfigComponent,
-                },
-            ],
-            {
-                slug: 'solid-server',
-            }
-        );
     };
 }
 
---
+++ addon/extension.js
@@ -0,0 +1,26 @@
+import { MenuItem, ExtensionComponent } from '@fleetbase/ember-core/contracts';
+
+export default {
+    setupExtension(app, universe) {
+        const menuService = universe.getService('menu');
+
+        // Register menu item in header
+        const iconOptions = { iconComponent: new ExtensionComponent('@fleetbase/solid-engine', 'solid-brand-icon'), iconComponentOptions: { width: 19, height: 19 } };
+        menuService.registerHeaderMenuItem('Solid', 'console.solid-protocol', { priority: 5 });
+
+        // Register admin settings -- create a solid server menu panel with it's own setting options
+        universe.registerAdminMenuPanel(
+            'Solid Protocol',
+            [
+                new MenuItem({
+                    title: 'Solid Server Config',
+                    icon: 'sliders',
+                    component: new ExtensionComponent('@fleetbase/solid-engine', 'admin/solid-server-config'),
+                }),
+            ],
+            {
+                slug: 'solid-server',
+            }
+        );
+    },
+};
